8d4a42c24a73932002dbab9a265372b38320535b
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2025-06-08 20:27:47 +1000

    add new parameters to task item model

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -3,7 +3,10 @@ namespace TaskMateAPI.Models
     public class TaskItem
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
-        public bool IsComplete { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsComplete { get; set; } = false;
     }
+
+
 }
