9905966238b65c9bdb25b646967d6f7cd7a82e5b
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2025-06-08 18:11:15 +1000

    remove extra attributes from task item class

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -3,9 +3,7 @@ namespace TaskMateAPI.Models
     public class TaskItem
     {
         public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string? Description { get; set; }
-        public bool IsCompleted { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string? Title { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
